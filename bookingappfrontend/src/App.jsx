eaa5559a924804261532d09d8373ba2e9545e7c4
mehanna <magedehanna@gmail.com>
2024-11-21 15:12:13 -0600

    Adding the homePage

--- bookingappfrontend/src/App.jsx
+++ bookingappfrontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import {
   Route,
   createBrowserRouter,
@@ -6,11 +7,13 @@ import {
   RouterProvider,
 } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
+import HomePage from './pages/HomePage'
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
     </Route>
   )
 );
