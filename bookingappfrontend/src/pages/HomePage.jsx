cfef6c8b278d6f7f483aac8c0995a2e83bbf7fc9
mehanna <magedehanna@gmail.com>
2024-11-21 16:12:17 -0600

    Adding the images

--- bookingappfrontend/src/pages/HomePage.jsx
+++ bookingappfrontend/src/pages/HomePage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import rooms from '../assets/data/rooms'
+import RoomCard from '../components/RoomCard'
+
+
+
 const HomePage = () => {
   return (
     <>
       {rooms.length > 0 ? 
-      ( rooms.map((room) => (<h3>{room.name}</h3>) ) ) : 
+      ( 
+        rooms.map((room) => (<RoomCard key={room.id} room={room} /> ) )
+      ) : 
       ( <h1>No Rooms Found</h1> )}
     </>
   )
