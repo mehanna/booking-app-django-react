df11a2a4251ae69f3d19c1c0741587daefd47177
Maged <magedehanna@gmail.com>
2024-11-21 20:25:13 -0600

    Adding the NotFoundPage

--- bookingappfrontend/src/App.jsx
+++ bookingappfrontend/src/App.jsx
@@ -8,12 +8,14 @@ import {
 } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
 import HomePage from './pages/HomePage'
+import NotFoundPage from './pages/NotFoundPage'
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
+      <Route path='*' element={<NotFoundPage />} />
     </Route>
   )
 );
--- bookingappfrontend/src/components/RoomCard.jsx
+++ bookingappfrontend/src/components/RoomCard.jsx
@@ -25,7 +25,7 @@ const RoomCard = ({room}) => {
             </p>
             <p className="text-sm text-gray-600">
             <span className="font-semibold text-gray-800"> Price:</span>
-            {room.price_per_hour}
+            ${room.price_per_hour}/hour
             </p>
         </div>
         </div>
@@ -33,7 +33,7 @@ const RoomCard = ({room}) => {
         className="flex flex-col sm:flex-row w-full sm:w-auto sm:space-x-2 mt-2 sm:mt-0"
         >
         <a
-            href="room.html"
+            href={`/rooms/${room.id}`}
             className="bg-blue-500 text-white px-4 py-2 rounded mb-2 sm:mb-0 w-full sm:w-auto text-center hover:bg-blue-700"
             >View Room</a
         >
