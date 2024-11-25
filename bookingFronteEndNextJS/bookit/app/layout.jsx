aa7dfbb3f25aa44355a6847055980f80bc4e384f
Maged <magedehanna@gmail.com>
2024-11-25 14:49:11 -0600

    Adding the header to next.jsx

--- bookingFronteEndNextJS/bookit/app/layout.jsx
+++ bookingFronteEndNextJS/bookit/app/layout.jsx
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google';
 import '@/assets/styles/globals.css'
+import Header from '@/components/Header';
+
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,9 +13,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={inter.className}
-      >
+      <body className={inter.className} >
+        <Header />
         <main className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
           {children}
         </main>
