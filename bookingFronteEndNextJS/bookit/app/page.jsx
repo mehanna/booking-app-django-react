2b90e22d6699b8d47d6e469b7a01c03fef233a43
Maged <magedehanna@gmail.com>
2024-11-27 01:43:07 -0600

    Adding the roomcard and  headding

--- bookingFronteEndNextJS/bookit/app/page.jsx
+++ bookingFronteEndNextJS/bookit/app/page.jsx
@@ -1,7 +1,55 @@
+"use client";
 import Image from "next/image";
+import Heading from "@/components/Heading";
+import RoomCard from "@/components/RoomCard";
+import {React,
+  useState,
+  useEffect,
+} from 'react'
+
+
 
 export default function Home() {
+    // creating a state variable to store the job listings
+    const[rooms,setRooms] = useState([]);
+    const[loading,setLoading] = useState(true);
+    ///////////////////////////////////////////////
+    ///-Fetching/////////////////
+    // async Fetching the jobs data from the API
+    useEffect(() => {
+      const fetchJobs = async () => {
+        try {
+          // Fetching the jobs data from the API
+          const response = await fetch(`http://localhost:8000/api/rooms/`);
+          console.log('Fetch rooms data response:', response.url);
+
+          // Parsing the response to get the data
+          const data = await response.json();
+          // Logging the data to the console
+          console.log('rooms data:', data);
+          // Setting the jobs state variable with the data
+          setRooms(data);
+        } catch (error) {
+          console.error('Fetch rooms data error:', error);
+        } finally {
+          // Setting the loading state variable to false
+          setLoading(false);
+          console.log('Fetch rooms data finally');
+        }
+      }
+      // Calling the fetchJobs function
+      fetchJobs();
+    }, []); // Empty dependency array to run the effect only once
+    
+
   return (
-    <> bookit</>
-  );
+    <>
+      <Heading title="Available Rooms" />
+      {rooms.length > 0 ? 
+      ( 
+        rooms.map((room) => (<RoomCard key={room.id} room={room} /> ) )
+      ) : 
+      ( <h1>No Rooms Found</h1> )}
+    </>
+  )
 }
