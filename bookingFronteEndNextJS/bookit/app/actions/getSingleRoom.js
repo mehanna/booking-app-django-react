188c00809cc3db90b8db196970f0756429e2eba2
Maged <magedehanna@gmail.com>
2024-11-27 02:51:37 -0600

    Adding the room page with singleroom action

---
+++ bookingFronteEndNextJS/bookit/app/actions/getSingleRoom.js
@@ -0,0 +1,30 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+async function getSingleRooms(id) {
+    try {
+        // Fetching the rooms data from the API
+        const response = await fetch(`http://localhost:8000/api/rooms/${id}/`);
+
+        // Parsing the response to get the data
+        const data = await response.json();
+        // Logging the data to the console
+        console.log('rooms data:', data);
+        
+        // revalidating the cache
+        // Revalidate the cache for this path
+        // todo: fix the revalidatePath function 
+        //revalidatePath('/', 'layout');
+
+        return data;
+    }
+    catch (error) {
+        console.error('Fetch rooms data error:', error);
+        // Redirecting to the error page
+        redirect('/error');
+    }
+}
+
+export default getSingleRooms;
