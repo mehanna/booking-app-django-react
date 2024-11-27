4841f56657039060cf048c4d50fe20e56618f431
Maged <magedehanna@gmail.com>
2024-11-27 16:49:18 -0600

    Protecting Routes for booking

---
+++ bookingFronteEndNextJS/bookit/app/bookings/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BookingsPage = () => {
+  return (
+    <>booking </>
+  )
+}
+
+export default BookingsPage
--- bookingFronteEndNextJS/bookit/middleware.js
+++ bookingFronteEndNextJS/bookit/middleware.js
@@ -4,16 +4,20 @@ import { NextResponse } from "next/server";
 export async function middleware(req, ev) {
   // Extract the href and pathname properties from the request's nextUrl object.
   const { pathname } = req.nextUrl;
-  
   // Log the pathname and href to the console for debugging purposes.
-  console.log('Middleware pathname:', pathname,'File:', __filename);
-  //console.debug('Middleware request href:', href);
+  console.log('**Middleware** :', pathname,'File:', __filename);
 
+  // start of my main code 
+  const isAuthenticated = false;
+  if (!isAuthenticated) {
+    return NextResponse.redirect(new URL('/login',req.url));
+  }
   // Continue to the next middleware or the actual request handler.
   return NextResponse.next();
 }
 
 // Configuration object for the middleware, specifying that it should run on the '/login' and '/register' routes.
 export const config = {
-  matcher: ['/login', '/register'],
+  //matcher: ['/login', '/register'],
+  matcher: ['/bookings'],
 };
