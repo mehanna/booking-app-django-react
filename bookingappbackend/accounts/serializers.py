2a261bf0a84da44a7a17d1dbba6d6afe87330a23
Maged <magedehanna@gmail.com>
2024-11-30 02:48:32 -0600

    Update the django backend to use the email for both username and email and added the name failed

--- bookingappbackend/accounts/serializers.py
+++ bookingappbackend/accounts/serializers.py
@@ -4,31 +4,39 @@ from django.contrib.auth import authenticate  # Importing the authenticate funct
 
 # User Serializer
 class UserSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source='first_name')  # Adding a name field
+
     class Meta:
         model = User  # Specifying the model to serialize
-        fields = ('id','email')  # Specifying the fields to include in the serialization
+        fields = ('id', 'username', 'email', 'name')  # Specifying the fields to include in the serialization
 
 # Register Serializer
 class RegisterSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source='first_name')  # Adding a name field
+
     class Meta:
         model = User  # Specifying the model to serialize
-        fields = ('id','email', 'password')  # Specifying the fields to include in the serialization
+        fields = ('id', 'username', 'email', 'password', 'name')  # Specifying the fields to include in the serialization
         extra_kwargs = {'password': {'write_only': True}}  # Making the password field write-only
 
     def create(self, validated_data):
         # Creating a new user with the validated data
-        user = User.objects.create_user( validated_data['email'], validated_data['password'])
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data['first_name']  # Adding the name field
+        )
         return user  # Returning the created user
 
 # Login Serializer
 class LoginSerializer(serializers.Serializer):
-    email = serializers.EmailField()  # get to email field
-    password = serializers.CharField()
+    username = serializers.CharField()  # Defining a username field
+    password = serializers.CharField()  # Defining a password field
 
     def validate(self, data):
-        email = data.get('email')
-        password = data.get('password')
-        user = authenticate(username=User.objects.get(email=email).username, password=password)
+        # Authenticating the user with the provided data
+        user = authenticate(**data)
         if user and user.is_active:
-            return user
-        raise serializers.ValidationError("Incorrect Credentials")
+            return user  # Returning the authenticated user if active
+        raise serializers.ValidationError("Incorrect Credentials")  # Raising an error if authentication fails
