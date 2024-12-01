60780cc15ada0e6d7ec7a68fe5f158054d312524
Maged <magedehanna@gmail.com>
2024-11-30 20:49:06 -0600

    Get the actual state of the auth in the middleware

--- bookingFronteEndNextJS/bookit/middleware.js
+++ bookingFronteEndNextJS/bookit/middleware.js
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import checkAuth from "./app/actions/checkAuth";
 
 // This is an asynchronous middleware function that takes a request (req) and an event (ev) as parameters.
 export async function middleware(req, ev) {
@@ -8,7 +9,7 @@ export async function middleware(req, ev) {
   console.log('**Middleware** :', pathname,'File:', __filename);
 
   // start of my main code 
-  const isAuthenticated = false;
+  const {isAuthenticated} = await checkAuth();
   if (!isAuthenticated) {
     return NextResponse.redirect(new URL('/login',req.url));
   }
