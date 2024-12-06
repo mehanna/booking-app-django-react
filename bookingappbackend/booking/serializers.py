51bd599aaa6c9e1feefa99742e619176989590f7
mehanna <magedehanna@gmail.com>
2024-12-05 18:32:25 -0600

    fix an issue with backend requiring the roon fukk data set for post request

--- bookingappbackend/booking/serializers.py
+++ bookingappbackend/booking/serializers.py
@@ -8,8 +8,20 @@ class RoomSerializer(serializers.ModelSerializer):
         
 
 class BookingSerializer(serializers.ModelSerializer):
-    room = RoomSerializer()
+    # The 'room' field is a PrimaryKeyRelatedField, which means it will be represented by its primary key (ID).
+    # The 'required=False' makes this field optional in the input data.
+    room = serializers.PrimaryKeyRelatedField(queryset=Room.objects.all(), required=False)
     
     class Meta:
+        # Specifies the model that this serializer is for.
         model = RoomBooking
+        # Includes all fields from the RoomBooking model in the serializer.
         fields = '__all__'
+    
+    def to_representation(self, instance):
+        # Converts the instance into a dictionary of primitive data types.
+        representation = super().to_representation(instance)
+        # If the 'room' field is present, it uses the RoomSerializer to include the entire room data.
+        # Otherwise, it sets the 'room' field to None.
+        representation['room'] = RoomSerializer(instance.room).data if instance.room else None
+        return representation
