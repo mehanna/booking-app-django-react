be69160f39a792c2203a5a6394de32a3c7a43e3e
Maged <magedehanna@gmail.com>
2024-12-07 01:25:13 -0600

    The configuration is complete for versel

--- bookingappbackend/bookingappbackend/settings.py
+++ bookingappbackend/bookingappbackend/settings.py
@@ -145,6 +145,10 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
