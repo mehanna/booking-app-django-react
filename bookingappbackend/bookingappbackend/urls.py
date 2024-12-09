1bc2841e93da4895e9d56ca74aa8b3804176eb35
Maged <magedehanna@gmail.com>
2024-12-08 20:25:55 -0600

    Bug: try to fix vercel again

--- bookingappbackend/bookingappbackend/urls.py
+++ bookingappbackend/bookingappbackend/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', include('booking.urls')),
     path('',include('accounts.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
