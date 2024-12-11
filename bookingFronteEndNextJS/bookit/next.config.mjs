fc3034e029c2dc62181c29619b1f8221ce0bb2a7
Maged <magedehanna@gmail.com>
2024-12-11 11:37:25 -0600

    Use appwrite to get the images

--- bookingFronteEndNextJS/bookit/next.config.mjs
+++ bookingFronteEndNextJS/bookit/next.config.mjs
@@ -3,7 +3,8 @@ const nextConfig = {
     // The `images` property is used to configure the domains from which images can be loaded.
     // Here, the `domains` array includes 'localhost', allowing images to be loaded from the local development server.
     images: {
-        domains: ['127.0.0.1'],
+        domains: ['cloud.appwrite.io'],
+        
       },
   };
   
